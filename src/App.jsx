a70fb506150dbbeea93c466852f37b617b88c27d
yara <yarathabet66@gmail.com>
2025-12-08 23:33:59 +0200

    Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import Footer from './components/footer.jsx'
 import Navbar from "./layouts/Navbar"
 import Category from './pages/Category.jsx'
 import { Routes, Route } from 'react-router-dom'
+import Home from './pages/home/Home.jsx'
 const App = () => {
   return (
     <>
       <Navbar />
+      <Home/>
       <Routes>
         <Route path="/Category" element={< Category />} />
       </Routes>
