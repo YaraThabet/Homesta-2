3b17b57ba7c2db171e4cd79e39750b874c8a53ca
yara <yarathabet66@gmail.com>
2025-12-09 00:16:07 +0200

    Modification to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,6 @@ const App = () => {
     <div className="flex flex-col min-h-screen">
       <Navbar />
       <Home/>
-      <Routes>
-        <Route path="/Category" element={< Category />} />
-      </Routes>
-
       <main className="flex-grow">
         <Routes>
           <Route path="/category" element={<Category />} />
