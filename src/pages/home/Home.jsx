f3f9e1a70cc241ec6aebf200115cd390f5407bc3
yara <yarathabet66@gmail.com>
2025-12-12 00:37:24 +0200

    Added Advantages in home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from './components/Hero'
+import Advantages from './components/Advantages'
 
 const Home = () => {
   return (
     <div>
       <Hero/>
+      <Advantages/>
     </div>
   )
 }
