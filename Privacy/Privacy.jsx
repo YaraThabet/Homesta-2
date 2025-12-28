3bed831fc7079b57dc0484b78eddb6956147f516
NadaNasr22 <70757051+NadaNasr22@users.noreply.github.com>
2025-12-28 20:19:58 +0200

    Add files via upload
    Add Privacy page

---
+++ Privacy/Privacy.jsx
@@ -0,0 +1,35 @@
+
+const Privacy = () => {
+  return (
+    
+    <div className="max-w-4xl mx-auto px-4 py-12 mt-30 ">
+      <section className="mb-10">
+
+        <h1 className="text-3xl font-medium mb-8"> Cancelation Policy</h1>
+        <p className="leading-relaxed font-normal" style={{color:'#A4A7AE'}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. . Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </section>
+
+      <section className="mb-10">
+        <h1 className="text-3xl font-medium mb-8">Terms & Condition</h1>
+        <p className="leading-relaxed font-normal" style={{color:'#A4A7AE'}}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </section>
+    </div>
+  );
+};
+export default Privacy;
